460ca5bb3c8728e944f2bf11a23c4f09141fcf88
Liz <pachaffy@gmail.com>
2019-10-09 12:43:23 +0200

    Implement start timer with the name command

--- src/main/java/com/codecool/timer/controller/TimerController.java
+++ src/main/java/com/codecool/timer/controller/TimerController.java
@@ -1,4 +1,41 @@
 package com.codecool.timer.controller;
 
+import com.codecool.timer.service.TimerService;
+import com.codecool.timer.view.View;
+
 public class TimerController {
+
+    private TimerService timerService;
+    private View view;
+
+    public TimerController(TimerService timerService, View view) {
+        this.timerService = timerService;
+        this.view = view;
+    }
+
+    private void handleStartCommand(String[] intention) {
+        String timerName = view.getTimerName(intention);
+        timerService.handleStartTimer(timerName);
+    }
+
+    public void run() {
+        String[] intention;
+        String timerName;
+        String command;
+
+        boolean isRunning = true;
+        while (isRunning) {
+            view.printText("Command?");
+            intention = view.getUserIntention();
+            command = view.getCommand(intention);
+            switch (command) {
+                case "start":
+                    handleStartCommand(intention);
+                    break;
+                default:
+                    System.out.println("No such option.");
+                    break;
+            }
+        }
+    }
 }
