4e757710adb7417705d7f5f06823987d581ba9e9
Liz <pachaffy@gmail.com>
2019-10-09 12:32:48 +0200

    Implement View

--- src/main/java/com/codecool/timer/view/View.java
+++ src/main/java/com/codecool/timer/view/View.java
@@ -1,4 +1,39 @@
 package com.codecool.timer.view;
 
+import java.util.Scanner;
+
 public class View {
+
+    private Scanner scanner;
+
+    public View() {
+        scanner = new Scanner(System.in);
+    }
+
+    private String getUserInput() {
+        return scanner.nextLine();
+    }
+
+    public void printText(String message) {
+        System.out.println(message);
+    }
+
+    public String getCommand(String[] command) {
+        if (command.length < 1) {
+            throw new IllegalArgumentException("No command provided.");
+        }
+        return command[0];
+    }
+
+    public String[] getUserIntention() {
+        String command = getUserInput().toLowerCase();
+        return command.split(" ");
+    }
+
+    public String getTimerName(String[] command) {
+        if (command.length < 2) {
+            throw new IllegalArgumentException("No timer name given");
+        }
+        return command[1];
+    }
 }
