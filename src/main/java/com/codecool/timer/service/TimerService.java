c8ee6cf152839d34aeb8f9afdcfa2458fafbc224
Liz <pachaffy@gmail.com>
2019-10-09 12:24:17 +0200

    Implement method that starts the timer in TimerService

--- src/main/java/com/codecool/timer/service/TimerService.java
+++ src/main/java/com/codecool/timer/service/TimerService.java
@@ -1,4 +1,27 @@
 package com.codecool.timer.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.codecool.timer.model.Timer;
+
 public class TimerService {
+
+    private List<Timer> timers;
+
+    public TimerService() {
+        this.timers = new ArrayList<>();
+    }
+
+    public void handleStartTimer(String timerName) {
+        Timer timer = new Timer(timerName);
+        if (isStoppedTimer(timer)) {
+            timers.remove(timer);
+        }
+        timers.add(timer);
+    }
+
+    private boolean isStoppedTimer(Timer timer) {
+        return timers.contains(timer) && timers.get(timers.indexOf(timer)).isInterrupted();
+    }
 }
